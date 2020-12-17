b37baa8dff1613471be0a187db003a394d4f4af2
Joseph Dubon <josephdubon@pm.me>
2020-12-16 21:45:11 -0800

    Added comments to fib.py solution.

--- numseq/fib.py
+++ numseq/fib.py
@@ -4,9 +4,14 @@ def fib(n):
     f1, f2 = 0, 1
     # ... then every number after is the sum of the two preceding numbers
     if n == 0:
+        # return first number
         return f1
     if n == 1:
+        # return second number
         return f2
+    # loop over temp variable and every number after, is the sum of the
+    # preceding numbers
     for _ in range(1, n):
         f1, f2 = f2, f1 + f2
+    # return second number
     return f2
